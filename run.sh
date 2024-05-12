c32ff8761eda4753a0a5a32d91bf7a84d619ec44
Vinicius Renan <vinyciux@gmail.com>
2024-05-12 15:29:12 -0300

    run.sh for MPEFT

--- run.sh
+++ run.sh
@@ -2,64 +2,13 @@
 pop=50
 gen=700
 
-problems="Inspiral_50.dot Montage_50.dot Sipht_60.dot" 
-algs="AGEMOEA"
-heur="HSIP"
-qtdExp=16
-idexec=15
-echo "Go"
-
-while [ $idexec -lt $qtdExp ]
-do
-    for h in $heur
-    do
-        for a in $algs
-        do
-            for problem in $problems
-            do
-                out_filename="out"$a"_"$h"_"$idexec"_"$problem
-                err_filename="err"$a"_"$h"_"$idexec"_"$problem
-                python3 app/main.py --idexec $idexec -p $problem -a $a -h $h --pop $pop --gen $gen
-            done
-        done
-    done
-    let idexec=$idexec+1;
-done
-
-problems="Inspiral_100.dot" 
-algs="AGEMOEA"
-heur="PEFT"
-qtdExp=18
-idexec=17
-echo "Go"
-
-while [ $idexec -lt $qtdExp ]
-do
-    for h in $heur
-    do
-        for a in $algs
-        do
-            for problem in $problems
-            do
-                out_filename="out"$a"_"$h"_"$idexec"_"$problem
-                err_filename="err"$a"_"$h"_"$idexec"_"$problem
-                python3 app/main.py --idexec $idexec -p $problem -a $a -h $h --pop $pop --gen $gen
-            done
-        done
-    done
-    let idexec=$idexec+1;
-done
-
-
 problems="CyberShake_100.dot Epigenomics_100.dot Inspiral_100.dot Montage_100.dot Sipht_100.dot Epigenomics_24.dot CyberShake_30.dot Epigenomics_46.dot Inspiral_30.dot Montage_25.dot Sipht_30.dot CyberShake_50.dot Inspiral_50.dot Montage_50.dot Sipht_60.dot" 
 algs="NSGA2 AGEMOEA"
 heur="HEFT PEFT HSIP"
-#heur="MPEFT"
-qtdExp=14
-idexec=13
+heur="MPEFT"
 echo "Go"
-qtdExp=0
-idexec=13
+qtdExp=8
+idexec=0
 
 while [ $idexec -lt $qtdExp ]
 do
